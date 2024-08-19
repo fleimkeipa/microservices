c219b4d7f35bac90386f5151ed9f8cefddc4c9bc
fleimkeipa <adem_sahin12@hotmail.com>
2024-08-19 22:41:27 +0300

    Delete redundants

--- main-service/cmd/main.go
+++ main-service/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"order-service/internal/api/routes"
-	"order-service/pkg/nats"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,9 +9,6 @@ import (
 func main() {
 	e := echo.New()
 	routes.SetupRoutes(e)
-
-	nc := nats.ConnectToNATS()
-	defer nc.Close()
 
 	e.Logger.Fatal(e.Start(":8080"))
 }

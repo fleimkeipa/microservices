e73dec40c680d3d8e1420b6c815292f5d91ab2bb
fleimkeipa <adem_sahin12@hotmail.com>
2024-08-21 02:28:32 +0300

    Fix NATS error handling in nats-service

--- nats-service/cmd/main.go
+++ nats-service/cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"nats-listener-service/internal/listener"
 	"nats-listener-service/pkg/nats"
 )
 
 func main() {
 	// Get NATS connection
-	nc := nats.ConnectToNATS()
+	nc, err := nats.ConnectToNATS()
+	if err != nil {
+		log.Fatalf("failed to connect NATS: %v", err)
+		return
+	}
 	defer nc.Close()
 
 	// Start ListenerService and listen "order.created"
